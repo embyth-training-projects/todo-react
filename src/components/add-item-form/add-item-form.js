08a20c2b8a1d0709ae5a2accb193b281ed27c6bf
Rostyslav Miniukov <thresh1337@gmail.com>
2021-01-05 18:43:32 +0200

    Adding ability to name new todo items

--- src/components/add-item-form/add-item-form.js
+++ src/components/add-item-form/add-item-form.js
@@ -3,11 +3,44 @@ import React, {Component} from 'react';
 import './add-item-form.css';
 
 export default class AddItemForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      textContent: ``,
+    };
+
+    this.onAddItemInputChange = this.onAddItemInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onAddItemInputChange(evt) {
+    this.setState({
+      textContent: evt.target.value
+    })
+  }
+
+  onFormSubmit(evt) {
+    evt.preventDefault();
+
+    const {textContent} = this.state;
+
+    if (textContent === ``) {
+      return;
+    }
+
+    this.props.onAddButtonClick(textContent);
+    this.setState({
+      textContent: ``
+    })
+  }
+
   render() {
     return (
-      <div className='add-item-form'>
-        <button type='button' className='btn btn-outline-secondary' onClick={() => this.props.onAddButtonClick(`hi`)}>Add item</button>
-      </div>
+      <form className='add-item-form d-flex'>
+        <input type='text' className='form-control' placeholder='What needs to be done?' onChange={this.onAddItemInputChange} value={this.state.textContent} />
+        <button type='button' className='btn btn-outline-secondary' onClick={this.onFormSubmit}>Add item</button>
+      </form>
     )
   }
 }
